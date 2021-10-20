6925e9d82a68048b0ee56bc7a4d6d3571f2e4118
gessi <gessilane.i3solucoes@gmail.com>
2021-10-20 10:29:11 -0300

    alteração no heros atualizada

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,34 +1,51 @@
 class HeroesController < ApplicationController
-  before_action :set_hero, only: [:show, :update, :destroy]
+  before_action :set_heros, only: [:show, :update, :destroy]
 
   # GET /heroes
   def index
-    @heroes = Hero.all
+    @heroes = Heros.all
 
     render json: @heroes
   end
 
   # GET /heroes/1
   def show
-    render json: @hero
+    render json: @heros
   end
 
   # POST /heroes
   def create
-    @hero = Hero.new(hero_params)
+    @heros = Heros.new(heros_params)
 
-    if @hero.save
-      render json: @hero, status: :created, location: @hero
+    if @heros.save
+      render json: @heros, status: :created, location: @heros
     else
-      render json: @hero.errors, status: :unprocessable_entity
+      render json: @heros.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /heroes/1
   def update
-    if @hero.update(hero_params)
-      render json: @hero
+    if @heros.update(heros_params)
+      render json: @heros
     else
-      render json: @hero.errors, status: :unprocessable_entity
+      render json: @heros.errors, status: :unprocessable_entity
     end
   end
+
+  # DELETE /heroes/1
+  def destroy
+    @heros.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_heros
+      @heros = Heros.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def heros_params
+      params.require(:heros).permit(:name)
+    end
+end
